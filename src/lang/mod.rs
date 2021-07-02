532674bda3989cc4880a1f930fdd7cc962e4b55c
Kevin COMBRIAT <kevin@sc0ville.com>
2021-07-02 18:10:33 +0900

    refactor(project): Tailwind generator is now Style generator!

--- src/lang/mod.rs
+++ src/lang/mod.rs
@@ -3,13 +3,22 @@ use std::str::FromStr;
 pub mod elm;
 pub mod purescript;
 pub mod rescript;
+pub mod rust;
 pub mod typescript;
+
+pub use elm::ElmTemplate;
+pub use purescript::PurescriptTemplate;
+pub use rescript::RescriptTemplate;
+pub use rescript::RescriptiTemplate;
+pub use rust::RustTemplate;
+pub use typescript::TypescriptTemplate;
 
 #[derive(Debug)]
 pub enum Lang {
     Elm,
     Purescript,
     Rescript,
+    Rust,
     Typescript,
 }
 
@@ -21,9 +30,10 @@ impl FromStr for Lang {
             "elm" => Ok(Lang::Elm),
             "purescript" => Ok(Lang::Purescript),
             "rescript" => Ok(Lang::Rescript),
+            "rust" => Ok(Lang::Rust),
             "typescript" => Ok(Lang::Typescript),
             unknown_lang => Err(format!(
-                "\"{}\" is not a valid lang, should be one of (elm|purescript|rescript|typescript)",
+                "\"{}\" is not a valid lang, should be one of (elm|purescript|rescript|rust|typescript)",
                 unknown_lang
             )),
         }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use lang::Lang;
 use log::info;
 use std::fs::create_dir_all;
 
-use crate::lang::elm::ElmTemplate;
-use crate::lang::purescript::PurescriptTemplate;
-use crate::lang::rescript::{RescriptTemplate, RescriptiTemplate};
-use crate::lang::typescript::TypescriptTemplate;
+use crate::lang::{
+    ElmTemplate, PurescriptTemplate, RescriptTemplate, RescriptiTemplate, RustTemplate,
+    TypescriptTemplate,
+};
 use crate::utils::{extract_classes_from_file, resolve_path, write_code_to_file};
 
 mod classes_parser;
@@ -15,7 +15,7 @@ mod lang;
 mod utils;
 
 #[derive(Clap, Debug)]
-#[clap(name = "tailwind-generator")]
+#[clap(name = "style-generator")]
 struct Opts {
     /// CSS file to parse and generate code from
     #[clap(short, long)]
@@ -26,7 +26,7 @@ struct Opts {
     output: String,
 
     /// Filename (without extension) used for the generated code
-    #[clap(short = 'f', long, default_value = "Tailwind")]
+    #[clap(short = 'f', long, default_value = "Output")]
     output_filename: String,
 
     /// Language used in generated code (elm|purescript|rescript|typescript)"
@@ -78,6 +78,12 @@ fn main() -> Result<()> {
             write_code_to_file(
                 RescriptiTemplate { classes },
                 resolve_path(output, output_filename, "resi")?,
+            )?;
+        }
+        Lang::Rust => {
+            write_code_to_file(
+                RustTemplate { classes },
+                resolve_path(output, output_filename, "rs")?,
             )?;
         }
         Lang::Typescript => {
