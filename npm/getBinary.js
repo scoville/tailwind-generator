30201577690e6b9d3438feaf56a339fc18ec2f61
Kevin COMBRIAT <kevin@sc0ville.com>
2021-07-02 19:18:45 +0900

    chore(npm): Attempt to make binaries accessible after npm install

---
+++ npm/getBinary.js
@@ -0,0 +1,26 @@
+const { Binary } = require("@cloudflare/binary-install");
+
+const os = require("os");
+
+function getPlatform() {
+  const type = os.type();
+  const arch = os.arch();
+
+  // if (type === "Windows_NT" && arch === "x64") return "win64";
+  // if (type === "Windows_NT") return "win32";
+  // if (type === "Linux" && arch === "x64") return "linux";
+  if (type === "Darwin" && arch === "x64") return "macos";
+
+  throw new Error(`Unsupported platform: ${type} ${arch}`);
+}
+
+function getBinary() {
+  const platform = getPlatform();
+  const version = require("../package.json").version;
+  const url = `https://github.com/scoville/tailwind-generator/releases/download/v${version}/style-generator-${platform}.tar.gz`;
+  const name = "style-generator";
+
+  return new Binary(url, { name });
+}
+
+module.exports = getBinary;
---
+++ npm/install.js
@@ -0,0 +1,3 @@
+const getBinary = require("./getBinary");
+
+getBinary().install();
